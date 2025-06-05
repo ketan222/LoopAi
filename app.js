9749f282a7e77fd0907d258cc1908848f384de06
Ketan222 <ketanmodgil@gmail.com>
2025-06-05 13:59:43 +0530

    4st

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const ingestionController = require("./controllers/ingestion");
-
-const app = express();
-app.use(express.json());
-
-app.use("/ingest", ingestionController.ingest);
-
-module.exports = app;
